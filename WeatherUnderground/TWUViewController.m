be7686d52f7d8dc0471637b86d5129d0ff5cdcfb
ctrevarthen <ct@detroitlabs.com>
2014-04-26 23:13:33 -0400

    Add a bunch of comments to the tests to explain what is going on at each step.

--- WeatherUnderground/TWUViewController.m
+++ WeatherUnderground/TWUViewController.m
@@ -27,23 +27,25 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-    [manager GET:@"http://api.wunderground.com/api/9ceac7e6eae5cd1e/conditions/q/MI/Detroit.json" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject)
-     {
-     NSURL *urlString = [NSURL URLWithString:responseObject[@"current_observation"][@"icon_url"]];
-     NSData *dataFromUrlString = [NSData dataWithContentsOfURL:urlString];
-     NSData *pngRep = UIImagePNGRepresentation([UIImage imageWithData:dataFromUrlString]);
-     NSData *imageData = [NSData dataWithData:pngRep];
-     UIImage *image = [UIImage imageWithData:imageData];
-     
-     self.conditionsIcon.image = image;
-     
-     self.conditionsLabel.text = responseObject[@"current_observation"][@"weather"];
-     self.temperatureLabel.text = responseObject[@"current_observation"][@"temperature_string"];
-     self.cityLabel.text = responseObject[@"current_observation"][@"display_location"][@"full"];
-     
-     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-         NSLog(@"Error: %@", error);
-     }];
+    [manager GET:@"http://api.wunderground.com/api/9ceac7e6eae5cd1e/conditions/q/MI/Detroit.json"
+      parameters:nil
+         success:^(AFHTTPRequestOperation *operation, id responseObject) {
+             NSURL *urlString = [NSURL URLWithString:responseObject[@"current_observation"][@"icon_url"]];
+             NSData *dataFromUrlString = [NSData dataWithContentsOfURL:urlString];
+             NSData *pngRep = UIImagePNGRepresentation([UIImage imageWithData:dataFromUrlString]);
+             NSData *imageData = [NSData dataWithData:pngRep];
+             UIImage *image = [UIImage imageWithData:imageData];
+             
+             self.conditionsIcon.image = image;
+             
+//             self.conditionsLabel.text = responseObject[@"current_observation"][@"weather"];
+//             self.temperatureLabel.text = responseObject[@"current_observation"][@"temperature_string"];
+//             self.cityLabel.text = responseObject[@"current_observation"][@"display_location"][@"full"];
+         }
+         failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+             NSLog(@"Error: %@", error);
+         }
+     ];
 }
 
 -(void)viewDidAppear:(BOOL)animated {
@@ -53,7 +55,6 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
--- WeatherUndergroundTests/TWUViewControllerSpec.m
+++ WeatherUndergroundTests/TWUViewControllerSpec.m
@@ -1,5 +1,5 @@
 //
-//  UITextField_Mine.m
+//  TWUViewControllerSpec.m
 //  WeatherUnderground
 //
 //  Created by Christopher Trevarthen on 4/25/14.
@@ -19,52 +19,78 @@
 NSData *mockPNGObject;
 
 NSData *UIImagePNGRepresentation(UIImage *image) {
-    mockPNGObject = [NSData nullMock];
     return mockPNGObject;
 }
 
-SPEC_BEGIN(UITextField_Mine)
+SPEC_BEGIN(TWUViewControllerSpec)
 
     describe(@"TWUViewController", ^{
         __block TWUViewController *viewController;
         beforeEach(^{
            viewController = [[TWUViewController alloc] init];
         });
-		context(@"when the user opens the app", ^{
-            
-            xit(@"sets the icon to an image", ^{
-                [viewController stub:@selector(conditionsIcon) andReturn:[UIImageView nullMock]];
-                [[viewController.conditionsIcon should] receive:@selector(setImage:)];
-                [viewController viewWillAppear:YES];
-            });
-            
-
+    
+        context(@"when the user opens the app", ^{
 
             it(@"sets the icon to the weather condition", ^{
                 
-//              Setting up the test for AFNetworking
+                // create a mock version of the afnetworking operation manager
                 AFHTTPRequestOperationManager *mockManager = [AFHTTPRequestOperationManager nullMock];
                 [AFHTTPRequestOperationManager stub:@selector(manager) andReturn:mockManager];
+                
+                // make sure the real manager code isn't called
                 [mockManager stub:@selector(GET:parameters:success:failure:)];
+                
+                // set up a spy to get the success block we pass to the manager
                 KWCaptureSpy *spy = [mockManager captureArgument:@selector(GET:parameters:success:failure:) atIndex:2];
                 
+                // call the actual method so that we can use the spy to capture the success block argument
                 [viewController viewWillAppear:YES];
                 
+                // we're going to force-call the success block, so we need to create some fake data to pass
                 NSDictionary *fakeResponse = @{@"current_observation": @{@"icon_url":@"http://icons-ak.wxug.com/i/c/k/rain.gif"}};
+                
+                // get the success block from the spy (have to call the view controller's method first to get this to populate
+                void (^successBlock)(AFHTTPRequestOperation *operation, id responseObject) = spy.argument;
+                
+                // expect that we'll create a URL given the fake response object data set above
+                // takes care of this line:
+                // NSURL *urlString = [NSURL URLWithString:responseObject[@"current_observation"][@"icon_url"]];
                 NSURL *mockURL = [NSURL nullMock];
                 [NSURL stub:@selector(URLWithString:) andReturn:mockURL withArguments:@"http://icons-ak.wxug.com/i/c/k/rain.gif"];
                 
+                // takes care of this line:
+                // NSData *dataFromUrlString = [NSData dataWithContentsOfURL:urlString];
                 NSData *mockData = [NSData nullMock];
                 [NSData stub:@selector(dataWithContentsOfURL:) andReturn:mockData withArguments:mockURL];
                 
+                // takes care of this portion of the line:
+                // [UIImage imageWithData:dataFromUrlString]
                 UIImage *mockDataImage = [UIImage nullMock];
                 [UIImage stub:@selector(imageWithData:) andReturn:mockDataImage withArguments:mockData];
+                
+                // takes care of this part of the line:
+                // NSData *pngRep = UIImagePNGRepresentation(...)
+                // and
+                // NSData *imageData = [NSData dataWithData:pngRep];
+                // by using our own UIImagePNGRepresentation method defined above to force it to return our own mock object
                 NSData *mockImageData = [NSData nullMock];
+                mockPNGObject = [NSData nullMock];
                 [NSData stub:@selector(dataWithData:) andReturn:mockImageData withArguments:mockPNGObject];
-                 UIImage *mockUIImage = [UIImage nullMock];
+                
+                // takes care of this line:
+                // UIImage *image = [UIImage imageWithData:imageData];
+                UIImage *mockUIImage = [UIImage nullMock];
                 [UIImage stub:@selector(imageWithData:) andReturn:mockUIImage withArguments:mockImageData];
-                void (^successBlock)(AFHTTPRequestOperation *operation, id responseObject) = spy.argument;
+
+                // create the expectation that we'll be setting the image for the icon
+                // (needed to make the conditionsIcon a nullMock because view items aren't always instantiated in tests like
+                // they are when running the app)
+                [viewController stub:@selector(conditionsIcon) andReturn:[UIImageView nullMock]];
                 [[viewController.conditionsIcon should] receive:@selector(setImage:) withArguments:mockUIImage];
+                
+                // call the success block directly, since that has the code we care about testing, which is setting the image.
+                // all of the expectations and mocks we set up above will be evaluated at this point.
                 successBlock(nil, fakeResponse);
             
                 
